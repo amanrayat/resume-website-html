223e932563237cffda34a4120334fd7c554a13b8
Aman Rayat <rayat.a@husky.neu.edu>
2018-05-09 15:44:14 +0530

    website

---
+++ src/js/main.js
@@ -0,0 +1,57 @@
+
+var downArrow = document.getElementById("btn1");
+var upArrow = document.getElementById("btn2");
+
+downArrow.onclick = function () {
+    'use strict';
+    document.getElementById("first-list").style = "top:-620px";
+    document.getElementById("second-list").style = "top:-620px";
+    downArrow.style = "display:none";
+    upArrow.style = "display:block";
+};
+
+upArrow.onclick = function () {
+    'use strict';
+    document.getElementById("first-list").style = "top:0";
+    document.getElementById("second-list").style = "top:80px";
+    upArrow.style = "display:none";
+    downArrow.style = "display:block";
+};
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
